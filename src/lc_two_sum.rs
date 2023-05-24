b30260afa45f21439a05456c13b9fb613395d0bb
jervw <vuolajere@gmail.com>
2023-05-24 09:05:29 +0300

    unify tests into one function

--- src/lc_two_sum.rs
+++ src/lc_two_sum.rs
@@ -13,16 +13,8 @@ pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
 }
 
 #[test]
-fn ex1() {
-    assert_eq!(two_sum(vec![2, 7, 11, 15], 9), vec![0, 1])
-}
-
-#[test]
-fn ex2() {
-    assert_eq!(two_sum(vec![3, 2, 4], 6), vec![1, 2])
-}
-
-#[test]
-fn ex3() {
-    assert_eq!(two_sum(vec![3, 3], 6), vec![0, 1])
+fn test() {
+    assert_eq!(two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
+    assert_eq!(two_sum(vec![3, 2, 4], 6), vec![1, 2]);
+    assert_eq!(two_sum(vec![3, 3], 6), vec![0, 1]);
 }
